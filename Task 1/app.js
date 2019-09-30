c725f058fb28878a286a7debe30726a6e4aca227
Yaroslav <zhuravlev.yaroslav@gmail.com>
2019-09-30 14:07:15 +0300

    first task

---
+++ Task 1/app.js
@@ -0,0 +1,27 @@
+let money = prompt("Ваш бюджет на месяц?","");
+let time = prompt("Введите дату в формате YYYY-MM-DD","");
+let expensesQ1 = prompt("Введите обязательную статью расходов в этом месяце","");
+let expensesQ2 = prompt("Во сколько обойдется?","");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {
+        expensesQ1: expensesQ2
+    },
+    optionalExpenses: "",
+    income: [],
+    savings: false
+}
+
+alert(money / 30);
+
+
+// Вопросы к этому заданию:
+
+// Сколько типов данных существует в JS?
+// 7 типов данных: String, Number, Boolean, Object, Null, Undefined, Symbol
+
+// Как вывести информацию в консоль?
+
+// Какая функция операторов || и &&?
