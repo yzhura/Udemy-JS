0c68ac7dc603f7dc0acb8589119ebb2a6e55b00e
Yaroslav <zhuravlev.yaroslav@gmail.com>
2019-10-01 11:38:59 +0300

    task 4

--- Task 1/app.js
+++ Task 1/app.js
@@ -1,6 +1,15 @@
+let money, time;
 
-let money = +prompt("Ваш бюджет на месяц?","");
-let time = prompt("Введите дату в формате YYYY-MM-DD","");
+function start() {
+    money = +prompt("Ваш бюджет на месяц?","");
+    time = prompt("Введите дату в формате YYYY-MM-DD","");
+
+    while(isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?","");
+    }
+}
+
+start();
 
 let appData = {
     budget: money,
@@ -8,63 +17,70 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 }
 
-for(let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце","");
-    let b = prompt("Во сколько обойдется?","");
+function chooseExpenses() {
+    for(let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце","");
+        let b = prompt("Во сколько обойдется?","");
+    
+        if( (typeof(a)) === 'string' && (typeof(a)) != null  && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+            console.log("done");
+            appData.expenses[a] = b;
+        } else {
+            i--;
+        }
+    };
+}
 
-    if( (typeof(a))=== 'string' && (typeof(a)) != null  && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
-        console.log("done");
-        appData.expenses[a] = b;
+chooseExpenses();
+
+function detectDayBudget() {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert("Eжедневный бюджет: " + appData.moneyPerDay);
+    if(appData.moneyPerDay < 100) {
+        console.log("Минимальный уровень достатка");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log("Средний уровень достатка");
+    } else if(appData.moneyPerDay > 2000 ) {
+        console.log("Высокий уровень достатка");
     } else {
-
+        console.log('Error');
     }
-};
-
-// let i = 0;
-// while(i < 2) {
-//     let a = prompt("Введите обязательную статью расходов в этом месяце","");
-//     let b = prompt("Во сколько обойдется?","");
-//     if( (typeof(a))=== 'string' && (typeof(a)) != null  && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
-//         appData.expenses[a] = b;
-//     } else {
-
-//     }
-//     i++;
-// }
-
-// let i = 0;
-// do {
-//     let a = prompt("Введите обязательную статью расходов в этом месяце","");
-//     let b = prompt("Во сколько обойдется?","");
-//     if( (typeof(a))=== 'string' && (typeof(a)) != null  && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
-//         appData.expenses[a] = b;
-//     } else {
-
-//     }
-//     i++;
-// } while (i < 2);
-
-
-appData.moneyPerDay = appData.budget / 30;
-
-alert("Eжедневный бюджет: " + appData.moneyPerDay);
-
-if(appData.moneyPerDay < 100) {
-    console.log("Минимальный уровень достатка");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log("Средний уровень достатка");
-} else if(appData.moneyPerDay > 2000 ) {
-    console.log("Высокий уровень достатка");
-} else {
-    console.log('Error');
 }
 
+detectDayBudget();
 
 
-// Вопросы к этому заданию:
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt("Какова сумма накоплений?", ""),
+            percent = +prompt("Под какой процент?", "");
+        appData.monthIncome = save/100/12*percent;
+        alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+    }
+}
+
+checkSavings();
+
+function detectLevel() {
+
+}
+
+function chooseOptExpenses() {
+    for(let i = 0; i < 3; i++) {
+        let a = prompt("Статья необязательных расходов?", "");
+
+        if( (typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50) {
+            appData.optionalExpenses[i + 1] = a;
+        } else {
+            i--;
+        }
+    };
+}
+
+chooseOptExpenses()
 
 // Сколько типов данных существует в JS?
 // 7 типов данных: String, Number, Boolean, Object, Null, Undefined, Symbol
@@ -75,6 +91,8 @@ if(appData.moneyPerDay < 100) {
 // Какая функция операторов || и &&?
 // || - возвращает если один из аргументов равен true
 // && - если ве аргументы равны true
+
+// Task 2
 
 // Сколько видов циклов существует в JS?
 // 3
