8300c428e4a4b71a1160e96212eb84a4db585e2f
Yaroslav <zhuravlev.yaroslav@gmail.com>
2019-10-09 12:13:25 +0300

    task 12

---
+++ Task 9/js/es6.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    class Options {
+        constructor(height, width, bg, fontSize, textAlign) {
+            this.height = height;
+            this.width = width;
+            this.bg = bg;
+            this.fontSize = fontSize;
+            this.textAlign = textAlign;
+        }
+        createNewDiv(anytext) {
+            let newDiv = document.createElement('div');
+            newDiv.innerHTML = `<p>${anytext}</p>`;
+            newDiv.style.cssText = `height: ${this.height}px; width: ${this.width}px; background: ${this.bg}; font-size: ${this.fontSize}px; text-align: ${this.textAlign};`;
+            document.body.prepend(newDiv);
+        }
+    }
+
+    let obj = new Options(50, 75, 'red', 15, 'center');
+    obj.createNewDiv('Привет');
+});
--- Task 9/js/script.js
+++ Task 9/js/script.js
@@ -33,7 +33,7 @@ window.addEventListener('DOMContentLoaded', function() {
                 }
             }
         }
-    })
+    });
 
 
     //TIMER
