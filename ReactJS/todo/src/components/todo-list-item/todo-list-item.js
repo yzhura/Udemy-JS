4a8b056b8085256fcf469d1e22faf1bd56441e84
Yaroslav <zhuravlev.yaroslav@gmail.com>
2019-10-31 19:01:02 +0200

    make 035s

---
+++ ReactJS/todo/src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import './todo-list-item.css';
+
+
+export default class TodoListItem extends Component {
+
+	constructor() {
+		super();
+
+		this.onLabelClick = () => {
+			console.log('Done');
+		}
+	}
+
+	render() {
+		const { label, important = false } = this.props;
+
+		const style = {
+			color: important ? 'steelblue' : 'black',
+			fontWeight: important ? 'bold' : 'normal'
+		  };
+		
+		  return (
+			<span className="todo-list-item">
+			  <span
+				className="todo-list-item-label"
+				style={style}
+				onClick={this.onLabelClick}>
+				{label}
+			  </span>
+		
+			  <button type="button"
+					  className="btn btn-outline-success btn-sm float-right">
+				<i className="fa fa-exclamation" />
+			  </button>
+		
+			  <button type="button"
+					  className="btn btn-outline-danger btn-sm float-right">
+				<i className="fa fa-trash-o" />
+			  </button>
+			</span>
+		  );
+	}
+}
--- ReactJS/todo/src/index.js
+++ ReactJS/todo/src/index.js
@@ -1,27 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list';
+import App from './components/app';
 
-const App = () => {
-
-    const todoData = [
-        {label: 'Drink', important: false},
-        {label: 'Eat', important: true},
-        {label: 'Sleep', important: true},
-        {label: 'Dimonaaaaaaaaaaaa', important: true},
-        {label: 'Ne Dimonkay', important: true},
-    ]
-
-    return (
-        <div>
-            <AppHeader></AppHeader>
-            <SearchPanel></SearchPanel>
-            <TodoList todos={todoData}></TodoList>
-        </div>
-    );
-};
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
