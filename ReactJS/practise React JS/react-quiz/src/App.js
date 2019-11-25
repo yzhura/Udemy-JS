08f3169d6ab0e6068ab7e7aca58b9d932dd53cb4
Yaroslav <zhuravlev.yaroslav@gmail.com>
2019-11-25 18:49:02 +0200

    done part 8. Add React Router to project

--- ReactJS/practise React JS/react-quiz/src/App.js
+++ ReactJS/practise React JS/react-quiz/src/App.js
@@ -10,9 +10,12 @@ import QuizList from './container/QuizList/QuizList';
 function App() {
 	return (
 		<Layout>
-			<Switch>
+			{/* Switch нужен для показа именно того компонента который зарегистрирован в Роуте (аналогия с роботой атрибута exact). Если не оборачивать все в кмпонент Switch и не писать в Route атрибут exact тогда все будет показано друг под другом (Попрбуй закоментить switch)*/}
+			<Switch> 
+				{/* Route - зарегестрированые пути для показа компонентов, для того что бы не перезагружалась страница нужно подключать NavLinks */}
 				<Route path='/auth' component={Auth}></Route>
 				<Route path='/quiz-creator' component={QuizCreator}></Route>
+				{/* path='/quiz/:id' --> динамический роут, рендерится в QuizList.js */}
 				<Route path='/quiz/:id' component={Quiz}></Route>
 				<Route path='/' component={QuizList}></Route>
 			</Switch>
