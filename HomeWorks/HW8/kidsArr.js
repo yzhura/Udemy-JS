7d996e764c2b486c5aad49af97f06534eea35a89
Yaroslav Zhuravlev <zhuravlev.yaroslav@gmail.com>
2020-02-10 22:47:01 +0200

    task8 git commit -m task8§

--- HomeWorks/HW8/kidsArr.js
+++ HomeWorks/HW8/kidsArr.js
@@ -21,10 +21,42 @@ const kidsArr = [
     },
 ];
 
-function counter(x) {
 
-};
+// Task 1
+
+function clos() {
+    let sum = 0;
+    return function(num) {
+        sum += num;
+        console.log(sum);
+        return sum;
+    }
+}
+
+const counter = clos();
 
 counter(3) // 3
 counter(5) // 8
 counter(228) // 236
+
+
+
+// Task 2
+
+function closArr() {
+    let arr = [];
+    return function(data) {
+        data ? arr.push(data) : arr.length = 0;
+        console.log(arr);
+        return arr;
+    }
+}
+
+let getUpdatedArr = closArr();
+
+
+getUpdatedArr(3) // [3]
+getUpdatedArr(5) // [3, 5]
+getUpdatedArr({name: 'Vasya'}) // [3, 5, {}]
+getUpdatedArr() // []
+getUpdatedArr(4) // [4]
